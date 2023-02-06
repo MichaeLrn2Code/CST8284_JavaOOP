2d78b4f54f5a4fc6d10d7baf721b189ed3e9c206
MichaeLrn2Code <michaelwai93.ca@gmail.com>
2023-02-06 00:30:42 -0500

    initial commit_lab2 and lab3
    Uploaded lab2 and lab3

---
+++ Lab 2/src/CovidStatistics.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+/**
+ * File name: Username.java
+ * Author: Wai Wai Chan, 041057469
+ * Course: CST8284 – Java
+ * Assignment: Lab 02
+ * Due Date: 29 Jan 2023
+ * Professor: David Houtman
+ * Purpose: This program is to show the number of patient across 7 provinces in Canada
+ * who have recovered from COVID-19 between Feb and Sept 2020.
+ */
+
+/**
+ * This class contains a 2D integer array to store the number of patient who
+ * have recovered from COVID-19 between Feb to Sept 2020 across 7 provinces in
+ * Canada, a 1D String array to store the name of provinces in Canada. Also
+ * using two nested for-loop to represent the number of recovered persons for
+ * each month and each provinces as well as compute the sum for each month.
+ *
+ * A 1D integer array to store the row sum of recovered patient for each province over 8 months,
+ * finally prompt user to enter the province number, 
+ * and then show the sum of recovered patient for corresponding province number.
+ * 
+ * @author Wai Wai Chan
+ * @see java.util.Scanner
+ * @version 1.0
+ */
+public class CovidStatistics {
+	/**
+	 * Main method, program starts from here.
+	 * 
+	 * @param args it is a String[] arguments not being used
+	 */
+	public static void main(String[] args) {
+		final int ROWS = 7;
+		final int COLUMNS = 8;
+		Scanner keyboard = new Scanner(System.in);
+		
+		//create a new array to store the row sum of recovered patient for each province
+		int[] rowSum = new int[7];
+
+		/* 2D array to store the number of recovered persons for each month and each
+		 * provinces
+		 */
+		int[][] patients = { 
+				{ 2200, 1100, 1200, 1000, 1015, 2000, 1092, 2204 },
+				{ 5020, 6105, 2009, 9047, 1016, 2014, 2708, 2308 }, 
+				{ 1720, 2406, 3054, 1018, 1023, 3100, 1406, 1502 },
+				{ 1490, 2002, 2016, 5008, 2044, 1055, 1607, 2201 }, 
+				{ 1520, 1007, 1092, 2065, 1023, 1010, 1046, 1502 },
+				{ 1670, 1201, 2008, 2001, 1086, 1009, 1041, 1706 }, 
+				{ 1870, 2001, 2078, 1006, 1053, 1702, 1009, 1406 }
+
+		};
+
+		// 1D array to store the name of province in Canada
+		String[] provinces = { "Ontario", "Quebec", "Nova Scotia", "New Brunswick", "Manitoba", "British Columbia",
+				"Prince Edward Island" };
+
+		System.out.println("Province Num       Month    Feb     March   April   May     June    July    Aug     Sept");
+		System.out.println();
+
+		/**
+		 * A nested for-loop to show the corresponding number of recovered persons in
+		 * each month for each province.
+		 * 
+		 * i is the outer loop control variable, to control outer loop to return the
+		 * name of provinces. j is the inner loop control variable, to control inner
+		 * loop to return the number of recovered patient for each month.
+		 */
+		int provinceSum = 0;
+		for (int i = 0; i < ROWS; i++) {
+			provinceSum = 0; // reset provinceSum to zero when start a new row
+			System.out.print(i);
+			System.out.printf("%23s", provinces[i]);
+			for (int j = 0; j < COLUMNS; j++) {
+				System.out.printf("%8d", patients[i][j]);
+				provinceSum += patients[i][j];
+			}
+			rowSum[i] = provinceSum;
+			System.out.println();
+		}
+
+		/**
+		 * A nested for-loop to compute the total sum of recovered people in each month.
+		 */
+		System.out.println();
+		System.out.print("     Recovered Patients  ");
+
+		for (int colNum = 0; colNum < COLUMNS; colNum++) {
+			int sum = 0; // reset sum to zero when start new outer-loop
+			for (int rowNum = 0; rowNum < ROWS; rowNum++) {
+				sum += patients[rowNum][colNum];
+			}
+			System.out.printf("%8d", sum);
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println("               Vaccinate and maintain good health practices!");
+		System.out.println();
+
+		// Prompt user to enter the province number
+		System.out.println("Enter the number (0 to 6) of the row to sum, "
+				+ "where 0 = Ontario, 1 = Quebec, and so on: ");
+		int userinput = keyboard.nextInt();
+
+		/* Show the sum of recovered patient over 8 months for corresponding province 
+		 * based on user input province number
+		 */
+		System.out.printf(
+				"The total number of recovered COVID cases was: %,d",rowSum[userinput]);
+		keyboard.close();
+	}
+}
